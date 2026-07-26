0f9f91fa0d12f7f9316ccbd00b1234fe6aab6476
Rizqi Wulandari <rizqi.2421400069@unuja.ac.id>
2026-07-26 18:26:37 +0700

    Update role requirements for transaction views

--- transaksi/views.py
+++ transaksi/views.py
@@ -7,7 +7,7 @@ from .models import Penjualan, DetailPenjualan
 
 
 @login_required
-@role_required('admin', 'kasir')
+@role_required( 'kasir')
 def buat_transaksi(request):
     keranjang = request.session.get('keranjang', {})
 
@@ -99,7 +99,7 @@ def struk(request, penjualan_id):
     return render(request, 'transaksi/struk.html', {'penjualan': penjualan})
 
 @login_required
-@role_required( 'kasir')
+@role_required('admin' , 'kasir')
 def riwayat_transaksi(request):
     penjualan_list = Penjualan.objects.all().order_by('-tanggal')
     return render(request, 'transaksi/riwayat_transaksi.html', {'penjualan_list': penjualan_list})
